8c5e08a337028593af3adb7ab63e4abc2a88f57e
Dorosin Alex-adrian <a.dorosin@reply.com>
2020-02-24 13:30:29 +0200

    update9

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,9 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 };
     }
 
+    handleResetClick() {
+        this.setState({quiz_position: 1});
+    }
 
     showNextQuestion() {
         this.setState({quiz_position: this.state.quiz_position + 1});
@@ -23,7 +26,7 @@ class Quiz extends Component {
         return (
             <div>
                 { isQuizEnd ? (
-                    <QuizEnd />
+                    <QuizEnd resetClickHandler={ this.handleResetClick.bind(this) }/>
                 ) : (
                     <QuizQuestion showNextQuestionHandler = {this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position -1 ]} className="QuizQuestion"/>    
                 )}
